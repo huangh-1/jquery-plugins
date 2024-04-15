05aa0f160b4f0804324f2a03b8d4be8220beac2a
huangh-1 <huangh1@tcnj.edu>
2024-04-15 15:58:57 -0400

    peep2

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,22 @@ if(jQuery) {
 else {
     console.log ("no jquery")
 }
+
+let mobileMenuOpen = false;
+$("#mobile-menu-button").click(function () {
+    //if mobile menu is closed, open it
+    if (mobileMenuOpen==false){
+        $("#main-menu").removeClass("display-none-mobile");
+        mobileMenuOpen = true;
+        $("mobile-menu-text").text("Close");
+        $("#mobile-menu-icon").attr("src","assets/images/close.svg");
+    }
+    //if mobile menu is opened, close it
+    else{
+        $("#main-menu").addClass("display-none-mobile");
+        mobileMenuOpen = false;
+        $("mobile-menu-text").text("Menu")
+        $("#mobile-menu-icon").attr("src","assets/images/menu.svg");
+    }
+    
+});
